3a9bb507549e41e58a5f7048f2c6c12abc2f1e62
Sergey Leshchenko <sergserg57@ukr.net>
2022-05-31 19:30:25 +0300

    Fix cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -66,7 +66,7 @@ namespace API
                 .AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials()
-                .WithOrigins("https://localhost:4200"));
+                .WithOrigins("https://localhost:4200", "https://appdatingdev.herokuapp.com"));
 
             app.UseAuthentication();
             app.UseAuthorization();
