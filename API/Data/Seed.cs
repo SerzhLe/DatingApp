4cfefe085d404f2ca689ec6ba59d5fd7d2143e7a
Sergey Leshchenko <sergserg57@ukr.net>
2022-06-01 11:50:59 +0300

    Save changed path

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -17,7 +17,7 @@ namespace API.Data
         {
             if (await userManager.Users.AnyAsync()) return;
 
-            var userData = await File.ReadAllTextAsync("userSeedData.json");
+            var userData = await File.ReadAllTextAsync(@"Data\Seed_Data\userSeedData.json");
 
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
             if (users == null) return;
