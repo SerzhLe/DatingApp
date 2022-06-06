1a3f37ba1ed255129832b0eab8b94737b69c191b
Sergiy Leshchenko <sergrut99@gmail.com>
2022-06-06 16:11:39 +0300

    Redeploy app to get last changes

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -17,6 +17,7 @@ namespace API.Data
         {
             if (await userManager.Users.AnyAsync()) return;
 
+            //change dob data
             var userData = await File.ReadAllTextAsync(@"Data/Seed_Data/userSeedData.json");
 
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
